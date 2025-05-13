554deecb6e0bac8f6ad0c7e47321b410b058e3cc
manuella <manurezendelourenco@gmail.com>
2025-05-13 11:06:08 -0300

    arrumando codigo do banco

--- codigo_banco.sql
+++ codigo_banco.sql
@@ -15,9 +15,7 @@ id_jovem INT NOT NULL,
 id_idoso INT NOT NULL,
 data_hora DATETIME NOT NULL,
 duracao INT NOT NULL,
-valor INT,
-confirmar_idoso BOOLEAN NOT NULL,
-confirmar_jovem BOOLEAN NOT NULL);
+);
  
 CREATE TABLE endereco ( 
 id_endereco INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,29 +29,29 @@ bairro VARCHAR(30) NOT NULL,
 cep VARCHAR(9) NOT NULL,
 pais VARCHAR(20) NOT NULL DEFAULT 'Brasil' );
 
-CREATE TABLE pagamento (
-id_pagamento INT AUTO_INCREMENT PRIMARY KEY,
-id_agendamento INT NOT NULL,
-status VARCHAR(20) NOT NULL );
+
 
 CREATE TABLE jovem (
 id_jovem INT AUTO_INCREMENT PRIMARY KEY,
 id_usuario INT NOT NULL,
 id_endereco INT NOT NULL,
 cpf VARCHAR(14) UNIQUE NOT NULL,
+valor_jovem DECIMAL(4,2),
 foto_jovem varchar(255),
+assinante_jovem BOOLEAN NOT NULL,
 data_nascimento DATE NOT NULL,
 experiencia TEXT,
-chave_pix VARCHAR(50),
 descricao VARCHAR(150) NOT NULL,
 telefone_jovem VARCHAR(14) UNIQUE NOT NULL,
 genero BOOLEAN NOT NULL);
+
 
 CREATE TABLE idoso (
 id_idoso INT AUTO_INCREMENT PRIMARY KEY,
 id_usuario INT NOT NULL,
 id_endereco INT NOT NULL,
 foto_idoso varchar(255),
+assinante_idoso BOOLEAN NOT NULL,
 cpf VARCHAR(14) UNIQUE NOT NULL,
 data_nascimento DATE NOT NULL,
 comorbidade BOOLEAN NOT NULL,
@@ -62,17 +60,13 @@ descricao VARCHAR(150) NOT NULL,
 telefone_idoso VARCHAR(14) UNIQUE NOT NULL,
 genero BOOLEAN NOT NULL);
 
-CREATE TABLE receber (
-id_receber INT AUTO_INCREMENT PRIMARY KEY,
-id_agendamento INT NOT NULL,
-status VARCHAR(20) NOT NULL,
-trabalho_realizado boolean not null );
 CREATE TABLE avaliacao (
 id_avaliacao int auto_increment primary key,
 id_agendamento int,
 nota decimal (3,2) not null,
 avaliacao varchar(150) not null
 );
+
 -- relacionar a tabela agendamento com a tabela jovem
 ALTER TABLE agendamento
 ADD CONSTRAINT `fk_agendamento_pk_jovem` 
@@ -84,18 +78,14 @@ ADD CONSTRAINT `fk_agendamento_pk_idoso`
 FOREIGN KEY agendamento(`id_idoso`)
 REFERENCES idoso(`id_idoso`);
  
--- relacionar a tabela pagamento com a tabela agendamento
-ALTER TABLE pagamento
-ADD CONSTRAINT `fk_pagamento_pk_agendamento` 
-FOREIGN KEY pagamento(`id_agendamento`)
-REFERENCES agendamento(`id_agendamento`);
+
 -- relacionar a tabela jovem com a tabela usuario
 ALTER TABLE jovem
 ADD CONSTRAINT `fk_jovem_pk_usuario` 
 FOREIGN KEY jovem(`id_usuario`)
 REFERENCES usuario(`id_usuario`);
  
--- relacionar a tabela jovem com a tabela usuario
+-- relacionar a tabela jovem com a tabela endereco
 ALTER TABLE jovem
 ADD CONSTRAINT `fk_jovem_pk_endereco` 
 FOREIGN KEY jovem(`id_endereco`)
@@ -110,12 +100,7 @@ ALTER TABLE idoso
 ADD CONSTRAINT `fk_idoso_pk_endereco` 
 FOREIGN KEY idoso(`id_endereco`)
 REFERENCES endereco(`id_endereco`);
--- relacionar a tabela receber com a tabela agendamento
-ALTER TABLE receber
-ADD CONSTRAINT `fk_receber_pk_agendamento` 
-FOREIGN KEY receber(`id_agendamento`)
-REFERENCES agendamento(`id_agendamento`);
- 
+
 -- relacionar a tabela avaliação com a tabela agendamento
 ALTER TABLE avaliacao
 ADD CONSTRAINT `fk_avalicao_pk_agendamento` 
